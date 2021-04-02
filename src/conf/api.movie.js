566e67ec76c3f8155cda9f27ab0d3698be24aed9
josselin krikorian <obarayego@gmail.com>
2021-04-02 19:07:45 +0200

    store creation for redux (doesnt work yet

--- src/conf/api.movie.js
+++ src/conf/api.movie.js
@@ -1,19 +1,31 @@
 import * as axios from 'axios';
 
-const apiMovie = axios.create({
+const unAuthapiMovie = axios.create( {
     baseURL: 'https://api.themoviedb.org/4'
-});
+} );
 
-apiMovie.interceptors.request.use(req => {
+export const apiMovie = unAuthapiMovie.interceptors.request.use( req => {
     req.headers[ 'Authorization' ] = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NjY1NTU4NDAzNjhjNTQ5MWNhZThkY2U3MzU5ZDAwMSIsInN1YiI6IjYwNTI2N2MzMjJlNDgwMDA2YWQ5ZDIwZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.q4Ei_0PF17kK8s1FsVrsL4KVlZXXbBy3ryXhlKHI23A';
     return req;
-});
+} );
 
-export default apiMovie;
 
-export const apiMovieMap = (m) => ({
+export const apiMovieMap = ( m ) => ( {
     img: `https://image.tmdb.org/t/p/w500${ m.poster_path }`,
     title: m.title,
     details: `${ m.release_date } | ${ m.vote_average }/10 | (${ m.vote_count })`,
     description: m.overview,
-});
+} );
+
+
+const apiMovieRequest = {
+    searchMovies: ( filter ) => {
+        const query = '?' + Object.keys( filter ).map(
+            k => `${ k }=${ filter[ k ] }&` ).join( '' );
+        return apiMovie.get( '/search/movie' + query )
+            .then( response => response.data.results )
+            .then( moviesApi => moviesApi.map( apiMovieMap ) );
+    }
+};
+
+export default apiMovieRequest;
---
+++ src/store/reducers.js
@@ -0,0 +1,13 @@
+// {
+//     movies: {
+//         date: [],
+//         selectedMovie: 0,
+//         isLoading: false,
+//         error: null,
+//     },
+//     favoris: {
+//         data: [],
+//         isloading: false,
+//         error: null,
+//     }
+// }
